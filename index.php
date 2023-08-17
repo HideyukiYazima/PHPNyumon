91d81f32091fdedcebb24a28f3dc88ad2352318b
Yazima <yazima04200@gmail.com>
2023-08-17 22:03:33 +0900

    上書き保存

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+    // いつものやつ
+    echo 'Hello, world!';
+  ?>
+  
+  <br>
+
+  <?php
+    // 14の結果を出す
+    echo 7 * 2;
+  ?>
+
+  <br>
+
+  <?php
+    // 2の結果を出す
+    echo 8 % 3;
+  ?>
+
+    // 12を出す
+  <?php
+    echo 5 + 7;
+  ?>
+
+  <br>
+
+    // 計算の文字列を出す
+  <?php
+    echo '5 + 7';
+  ?>
+
+  <!-- 変数$fruitを書いていきましょう -->
+  <?php
+    $fruit = 'りんご';
+    echo $fruit;
+  ?>
+
+  <br>
+
+  <!-- 変数$sumを書いていきましょう -->
+  <?php
+    $sum = 8 + 9;
+    echo $sum;
+  ?>
+
+</body>
+</html>
